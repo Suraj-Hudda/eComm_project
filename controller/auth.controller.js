a3dddbcdb098ae8ce6ab1a3e3f9394cccb7c4bcc
Suraj-Hudda <surajhudda17@gmail.com>
2024-05-24 11:37:42 +0530

    auth

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -3,6 +3,8 @@
  */
 const bcrypt=require("bcryptjs")
 const user_model=require("../models/user.model")
+const jwt=require("jsonwebtoken")
+const secret=require("../configrations/auth.config")
 exports.signup=async(req,res)=>{
     /**
      * logic to register the user
@@ -20,7 +22,15 @@ exports.signup=async(req,res)=>{
     }
     try{
          const user_created=await user_model.created(userObj)
-        //  return this user 
+        //  return this user
+        const res_obj={
+            name:user_created.name,
+            userId:user_created.userId,
+            email:user_created.email,
+            userType:user_created.userType,
+            CreatedAt:user_created.CreatedAt,
+            updatedAt:user_created.updatedAt
+        }
         res.status(201).send(user_created)
 
     }catch(err){
@@ -31,3 +41,31 @@ exports.signup=async(req,res)=>{
     }
     // 3. Return the response back to the user
 }
+    exports.signup=async (req,res)=>{
+        // check if the user id is present in the system
+        const user=await user_model.findOne({userId:req.body.userId})
+        if(user==null){
+           return res.status(400).send({
+                messege:"user id passed is not a valid user id"
+            })
+        }
+        // password is correct
+        isPasswordValid=bcrypt.compareSync(req.body.password,user.password)
+        if(!isPasswordValid){
+           return res.status(401).send({
+                messege:"password is incorrect"
+            })
+        }
+        // using jwt we will create the access token with a given TTL and return
+        const token=jwt.sign({id:user.userId},secret.secret,{
+            expiresIn:120,
+        })
+
+        res.status(200).send({
+            name:user.name,
+            userId:user.userId,
+            email:user.email,
+            userType:user.userType,
+            accessToken:token
+        })
+    }
